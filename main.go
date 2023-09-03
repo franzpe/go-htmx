96009e8b70226799d86537e22254c82066a9a9a0
František Poboček <pobocekfrantisek@gmail.com>
2023-09-03 13:37:06 +0200

    context and handlers

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"html/template"
 	"log"
 	"net/http"
@@ -33,6 +34,54 @@ func (c *Counter) GetValue() int {
 	return c.value
 }
 
+type CtxData struct {
+	template *template.Template
+	counter  *Counter
+}
+
+func HandleGet(w http.ResponseWriter, r *http.Request) {
+	appData := r.Context().Value("ctxData").(CtxData)
+
+	data := map[string]int{
+		"CounterValue": appData.counter.GetValue(),
+	}
+
+	appData.template.ExecuteTemplate(w, "index.html", data)
+}
+
+func HandleIncrease(w http.ResponseWriter, r *http.Request) {
+	appData := r.Context().Value("ctxData").(CtxData)
+
+	appData.counter.Increase()
+
+	data := map[string]int{
+		"CounterValue": appData.counter.GetValue(),
+	}
+
+	appData.template.ExecuteTemplate(w, "counter.html", data)
+}
+
+func HandleDecrease(w http.ResponseWriter, r *http.Request) {
+	appData := r.Context().Value("ctxData").(CtxData)
+
+	appData.counter.Decrease()
+
+	data := map[string]int{
+		"CounterValue": appData.counter.GetValue(),
+	}
+
+	appData.template.ExecuteTemplate(w, "counter.html", data)
+}
+
+func MainCtx(counter *Counter, tmpl *template.Template) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			ctx := context.WithValue(r.Context(), "ctxData", CtxData{counter: counter, template: tmpl})
+			next.ServeHTTP(w, r.WithContext(ctx))
+		})
+	}
+}
+
 func main() {
 	counter := &Counter{}
 	r := chi.NewRouter()
@@ -44,31 +93,11 @@ func main() {
 		log.Fatalf("unable to parse templates %e\n", err)
 	}
 
-	r.Get("/", func(w http.ResponseWriter, _ *http.Request) {
-		data := map[string]int{
-			"CounterValue": counter.GetValue(),
-		}
+	r.Use(MainCtx(counter, tmpl))
 
-		tmpl.ExecuteTemplate(w, "index.html", data)
-	})
-
-	r.Post("/increase", func(w http.ResponseWriter, _ *http.Request) {
-		counter.Increase()
-		data := map[string]int{
-			"CounterValue": counter.GetValue(),
-		}
-		tmpl.ExecuteTemplate(w, "counter.html", data)
-	})
-
-	r.Post("/decrease", func(w http.ResponseWriter, _ *http.Request) {
-		counter.Decrease()
-
-		data := map[string]int{
-			"CounterValue": counter.GetValue(),
-		}
-
-		tmpl.ExecuteTemplate(w, "counter.html", data)
-	})
+	r.Get("/", HandleGet)
+	r.Post("/increase", HandleIncrease)
+	r.Post("/decrease", HandleDecrease)
 
 	http.ListenAndServe("localhost:3000", r)
 }
